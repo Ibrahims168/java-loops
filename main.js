1c2806f2cece0aab011de662fc3a8f1ecb8a1b59
ibrahims168 <ibrahh881@gmail.com>
2022-02-03 17:00:44 +0200

    newjavaloops

---
+++ main.js
@@ -0,0 +1,25 @@
+// let count = 1000
+// let i=0
+// while (count >= 0.1) {
+//    console.log(count)
+    // count = count + 1 
+    // count += 1
+    // count *= 2
+//    count /= 2
+//    i++
+// }
+
+// let msg = 'finished executing the loop in '
+// msg+=i + ' iterations'
+// console.log(msg);
+
+
+const limit = 10
+let count = 1 
+let stars = "*"
+while (count <= limit){
+    console.log(stars);
+    stars += "*"
+    count++
+}
+
